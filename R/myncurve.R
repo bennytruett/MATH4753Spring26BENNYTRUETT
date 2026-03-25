50cbf52afd150a4c5039d23bbd91930accae9044
Benny Truett <benny.tru@ou.edu>
2026-03-25 15:08:32 -0500

    Make fire data

--- R/myncurve.R
+++ R/myncurve.R
@@ -19,19 +19,3 @@ myncurve = function(mu, sigma, a){
   prob=pnorm(a,mean=mu,sd=sigma)
   list(mu = mu, sigma = sigma, a = a, probability = prob)
 }
-
-library(testthat)
-test_that("mu is stored correctly", {
-  result <- myncurve(10,4,12)
-  expect_equal(result$mu, 10)
-})
-
-test_that("sigma is stored correctly", {
-  result <- myncurve(10,4,12)
-  expect_equal(result$sigma, 4)
-})
-
-test_that("probability is correct", {
-  result <- myncurve(0,1,0)
-  expect_equal(result$probability, 0.5)
-})
